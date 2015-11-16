c7d148acd295394154dcf8cfe64f762f57a27a87
Inoue-h <Inoue@test.com>
2015-11-16 05:02:08 +0000

    LineItem,cartに機能追加

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,20 @@ class StoreController < ApplicationController
 
   def index
     @products = Product.order(:title)
+
+    puts "------------------"
+    puts session[:counter]
+    puts "------------------"
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter]+=1
+    end
+
+    if session[:counter] < 5
+    else
+    @countup = "アクセスは#{session[:counter]}回目です。"
+    end
   end
 end
